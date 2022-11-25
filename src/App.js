f6558c30d8cb13d4a8c8008add62bc79c90933cb
Nitish Kumar <nk98721000@gmail.com>
2022-11-25 19:57:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { loads } from './features/water/waterSlice';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const route = useSelector(state => state.page);
+    document.title = route.title;
+
+    const dispatch = useDispatch();
+    dispatch(loads());
+
+    return (
+        <>
+            {route.page === '/'? <Home /> : ''}
+            {route.page === '/login'? <Login /> : ''}
+            {route.page === '/signup'? <Signup /> : ''}
+        </>
+    );
 }
 
 export default App;
